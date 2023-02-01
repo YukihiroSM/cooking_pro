753f7c710589c829038ee31e6932eebb47ec55eb
ruslankotliar <ruslankot101@knu.ua>
2023-02-01 23:18:21 +0100

    Created hero

--- frontend/src/layouts/main.layout.tsx
+++ frontend/src/layouts/main.layout.tsx
@@ -2,6 +2,7 @@ import { HeaderComponent } from './header.component';
 import { FooterComponent } from './footer.component';
 
 import React from 'react';
+import { Container } from '@chakra-ui/react';
 
 interface Props {
   children: React.ReactNode;
@@ -13,7 +14,19 @@ const LayoutComponent: React.FunctionComponent<Props> = ({
   return (
     <>
       <HeaderComponent />
-      <div>{children}</div>
+      <Container
+        as='main'
+        display={'flex'}
+        flexDirection={'column'}
+        alignItems={'center'}
+        bg={'light'}
+        pt={'10rem'}
+        pb={'1.5rem'}
+        margin={'auto'}
+        maxWidth={'100vw'}
+      >
+        {children}
+      </Container>
       <FooterComponent />
     </>
   );
