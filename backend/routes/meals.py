8ff5cb84ef1c78cd75de84a648868f348fcd0a9e
Dichik <omeluan.dima@gmail.com>
2023-02-02 11:37:32 +0200

    Add possibility to get list of meals

--- backend/routes/meals.py
+++ backend/routes/meals.py
@@ -3,11 +3,12 @@ import requests
 from fastapi import APIRouter
 
 from constants.constants import BASE_URL
+from entity.Category import Category
 
 from entity.Ingredient import Ingredient
 from entity.Meal import Meal
 
-router = APIRouter(prefix="/meals")
+router = APIRouter(prefix="/api/v1/meals")
 
 
 @router.get("/random")
@@ -15,6 +16,44 @@ async def getRandomMeal():
     url = BASE_URL + "/random.php"
     response = requests.get(url)
     data = response.json()["meals"][0]
+    return buildMeal(data)
+
+
+@router.get("/categories")
+async def getAllCategories():
+    name = "categories"
+    url = BASE_URL + f"/{name}.php"
+    response = requests.get(url)
+    data = response.json()[name]
+    categories = []
+    for category in data:
+        categories.append(buildCategory(category))
+    return categories
+
+
+def buildCategory(category):
+    return Category(
+        category["idCategory"],
+        category["strCategory"],
+        category["strCategoryDescription"]
+    )
+
+
+@router.get("/")
+async def getRandomMeals():
+    # FIXME move key to .env files
+    url = "https://www.themealdb.com/api/json/v2/9973533/randomselection.php"
+    response = requests.get(url)
+    data = response.json()["meals"]
+    meals = []
+    for m in data:
+        meal = buildMeal(m)
+        meals.append(meal)
+    return meals
+
+
+# TODO move to MealService
+def buildMeal(data):
     ingredients = []
     for key in data.keys():
         if key.startswith("strIngredient") and data[key]:
@@ -31,5 +70,3 @@ async def getRandomMeal():
         data["strInstructions"],
         ingredients
     )
-
-
