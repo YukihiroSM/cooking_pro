6f516882d1a6010dfaa852ef5cab69182b652602
Teasotea <sofi16616@gmail.com>
2023-02-06 03:31:26 +0200

    add auth endpoints and decode_jwt

--- backend/jwt_auth.py
+++ backend/jwt_auth.py
@@ -1,9 +1,18 @@
+from typing import Dict
 import jwt
+import hashlib
 
 SECRET_KEY = "my_secret_key"
 ALGORITHM = "HS256"
 
 
-def get_encoded_jwt(data):
+def get_encoded_jwt(data: Dict) -> str:
     encoded_jwt = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def decode_jwt(jwt: str) -> Dict:
+    decoded_jwt = jwt.decode(jwt, SECRET_KEY, algorithm=ALGORITHM)
+    return decoded_jwt
+
+def verify_password(hashed_password: str, password: str) -> bool:
+    return hashlib.sha256(password.encode()).hexdigest() == hashed_password
