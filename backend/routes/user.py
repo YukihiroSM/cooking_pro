02f0c94b7b6c6818bb1322309b0e4462bd2b1145
Andrii Smidonov <yukihirosm@gmail.com>
2023-02-07 14:43:55 +0200

    Add user possible meals endpoint

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,12 +1,14 @@
 import hashlib
 import pickle
 
+import requests
 from bson.objectid import ObjectId
 from fastapi import APIRouter, Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
 import jwt_auth
+from constants import MEAL_API_BASE_URL
 from schemas import AuthItem, UserIngredientCreation
 
 router = APIRouter(prefix="/api/user")
@@ -132,15 +134,36 @@ async def delete_user_ingredient(request: Request, user_id: str, ingredient_id: 
 
     return JSONResponse({"message": "Ingredient not found!"}, status_code=404)
 
-# @router.get("/{user_id}/meals")
-# def get_possible_meals(request: Request, user_id: str, page: int=0, perPage: int=12):
-#     token = jwt_auth.get_authorisation(request)
-#     if token is None:
-#         return JSONResponse({"message": "User not authorised!"}, status_code=401)
-#
-#     user = request.app.database.users.find_one({"_id": ObjectId(user_id)})
-#     if user is None:
-#         return JSONResponse({"message": "User not found!"}, status_code=404)
-#
-#     ingredients = pickle.loads(user.get("ingredients"))
-#     ingredients_names = ",".join([ingredient.get("label") for ingredient in ingredients])
+
+@router.get("/{user_id}/meals")
+def get_possible_meals(request: Request, user_id: str, page: int = 0, perPage: int = 12):
+    token = jwt_auth.get_authorisation(request)
+    if token is None:
+        return JSONResponse({"message": "User not authorised!"}, status_code=401)
+
+    user = request.app.database.users.find_one({"_id": ObjectId(user_id)})
+    if user is None:
+        return JSONResponse({"message": "User not found!"}, status_code=404)
+
+    ingredients = pickle.loads(user.get("ingredients"))
+    user_ingredients_names = [ingredient.get("label") for ingredient in ingredients]
+    suitable_meals = []
+    for ingredient in user_ingredients_names:
+        url = f"{MEAL_API_BASE_URL}/filter.php?i={ingredient}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            meals = response.json().get("meals")
+            if meals is not None:
+                for meal in meals:
+                    single_meal = dict()
+                    single_meal["ingredients"] = []
+                    for i in range(1, 21):
+                        ingredient = meal.get(f"strIngredient{i}")
+                        if ingredient is not None:
+                            single_meal["ingredients"].append(ingredient)
+                    single_meal["id"] = meal.get("idMeal")
+                    if sum(item in single_meal["ingredients"] for item in user_ingredients_names) >= 3:
+                        suitable_meals.append(single_meal)
+                    else:
+                        continue
+    print(suitable_meals)
